33de126f729bc169be3dd443996b9caa651f564f
Pavel Brodsky <mcouthon@users.noreply.github.com>
2021-03-02 12:21:31 +0200

    Add client/stories/js-template.stories.js

---
+++ client/stories/js-template.stories.js
@@ -0,0 +1,11 @@
+import { html } from 'lit-html';
+import '../src/'engineering_dojo'.js';
+
+export default {
+  title: ''engineering_dojo'',
+};
+
+export const App = () =>
+  html`
+    <'engineering_dojo'></'engineering_dojo'>
+  `;

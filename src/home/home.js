0d11bb724e2cbb947a3e62dfc7919850090a1fb5
Pavel Brodsky <pavel.brodsky@forter.com>
2021-03-03 16:03:41 +0200

    Copywriting

--- src/home/home.js
+++ src/home/home.js
@@ -2,23 +2,23 @@ import {
     Link
 } from "react-router-dom";
 
-export default function Home(){
+export default function Home() {
     return <>
-            <h1>You strive to get better, don't you?</h1>
-            <h2>But "getting better" is so subjective. How can one even know or tell others what better is? Instead, we tried to be helpful. </h2>
-            <h2>We’ll share with you ideas, concepts, frameworks and resources that can help you level up as a Software Engineer. </h2>
+        <h1>You strive to get better, right?</h1>
+        <h2>But "getting better" is so subjective. How do you even know or tell others what "better" is? So, we decided to be helpful. </h2>
+        <h2>We’re sharing here our ideas, concepts, frameworks and resources that will help you level up as a Software Engineer. </h2>
 
-            <div className="call-to-action">
-                <Link to="/questionnaire"><button> I’m ready, show me </button></Link>
-                <label>all free, no emails, no BS. We’re Engineers.</label>
-            </div>
+        <div className="call-to-action">
+            <Link to="/questionnaire"><button> I’m ready, show me </button></Link>
+            <label>all free, no emails, no BS. We’re Engineers.</label>
+        </div>
 
-            <h2> From the creators of other fun things </h2>
+        <h2> From the creators of other fun things </h2>
 
-            <div className="card-group">
-                <div className="card"> softwareArchitectureAddict.com </div>
-                <div className="card"> Forker video </div>
-                <div className="card"> Chuckwho.com </div>
-            </div>
-        </>;
+        <div className="card-group">
+            <div className="card"> softwareArchitectureAddict.com </div>
+            <div className="card"> Forker video </div>
+            <div className="card"> Chuckwho.com </div>
+        </div>
+    </>;
 }
--- src/questionnaire/quizQuestions.js
+++ src/questionnaire/quizQuestions.js
@@ -8,10 +8,10 @@ export const ROLES = {
 
 export var quizQuestions = [
     {
-        question: "When it’s unclear why you need to build a certain capability, what is your preference?",
+        question: "When it’s unclear why you need to build a certain capability, what do you do?",
         answers: [
             {
-                content: "As long as I understand the task, it’s fine for me",
+                content: "As long as I understand the task, it’s fine with me",
                 type: ROLES.ENTRY
             },
             {
@@ -19,37 +19,37 @@ export var quizQuestions = [
                 type: ROLES.NORMAL
             },
             {
-                content: "I feel comfortable talking with various people (product, sales, etc.) and figure out the answer",
+                content: "I feel comfortable talking with various people (product, sales, etc.) and figuring out the answer",
                 type: ROLES.SENIOR
             },
             {
-                content: "I’m usually the one who figure this thing out for others. I cannot work without it.",
+                content: "I’m usually the one who figure these thing out for others. That's how I work",
                 type: ROLES.PRINCIPLE
             }
         ]
     },
     {
-        question: "When you lead projects, how big are they?",
+        question: "When you lead projects, how big are they (number of people involved / time)?",
         answers: [
             {
-                content: "I help with other projects, still didn’t lead one myself",
+                content: "I help with other projects; haven't lead one myself yet",
                 type: ROLES.ENTRY
             },
             {
-                content: "I’m working on them myself, for a few weeks",
+                content: "I work by myself / up to a few weeks",
                 type: ROLES.NORMAL
             },
             {
-                content: "Me and 2-3 more people from my team, for a few weeks to a few months",
+                content: "Me and 2-3 more people from my team / a few weeks to a few months",
                 type: ROLES.SENIOR
             },
             {
                 content: "Me and 5-15 people from various teams, for a few weeks to a few months",
-                type: ROLES.PRINCIPLE
+                type: ROLES.STAFF
             },
             {
                 content: "I lead some of the biggest initiatives in the company, having at least 10 people working with me for more than a year",
-                type: ROLES.STAFF
+                type: ROLES.PRINCIPLE
             }
         ]
     },
@@ -65,57 +65,57 @@ export var quizQuestions = [
                 type: ROLES.NORMAL
             },
             {
-                content: "I can explain the goals and why they’re critical for our success to other teammates\n",
+                content: "I can explain the goals and why they’re critical for our success to other teammates",
                 type: ROLES.SENIOR
             },
             {
-                content: "I use company’s goals to attract talent, use it during interviews and help onboarding new teammates\n",
+                content: "I use the company’s goals to attract talent during interviews and while helping to onboard new teammates",
                 type: ROLES.PRINCIPLE
             }
         ]
     },
     {
-        question: "When others usually hear from you?",
+        question: "Who usually hears from you?",
         answers: [
             {
                 content: "I update my manager on my progress.",
                 type: ROLES.ENTRY
             },
             {
-                content: "Mostly within my team. I provide context on my work.",
+                content: "My teammates. I provide context on my work.",
                 type: ROLES.NORMAL
             },
             {
-                content: "I tend to update on bigger forums around the projects I lead, or things I did to make others’ life easier",
+                content: "I tend to update bigger forums around the projects I lead, or things I did to make others’ life easier",
                 type: ROLES.SENIOR
             },
             {
-                content: "It’s very common to hear from me on various projects and initiatives on email, Slack and f2f. Many people outside of my team knows my work.",
+                content: "It’s very common to hear from me on various projects and initiatives by email, Slack and f2f. Many people outside of my team know my work.",
                 type: ROLES.PRINCIPLE
             }
         ]
     },
     {
-        question: "When others working on a big project (>6 months effort), when are they coming to consult with you?",
+        question: "If others are working on a big project (>6 months effort), when are they coming to consult with you?",
         answers: [
             {
                 content: "Very rarely. Maybe if it’s around code I wrote.",
                 type: ROLES.ENTRY
             },
             {
-                content: "Here and there. I do have some expertise in specific types of systems or solutions, that people want o leverage my knowledge",
+                content: "Here and there. I do have some expertise in specific types of systems or solutions that people want to leverage my knowledge for",
                 type: ROLES.NORMAL
             },
             {
-                content: "People consult with me on big projects where I have context around - from making sure requirements are solid to the solutions and tradeoffs they have in mind",
+                content: "People consult with me on big projects where I have context - from making sure requirements are solid to the solutions and tradeoffs they have in mind",
                 type: ROLES.SENIOR
             },
             {
-                content: "People consult with me on everything they feel is a big challenge. Even if I don’t have the context, they feel comfortable sharing and getting my perspective.",
+                content: "People consult with me on everything they feel is a big challenge. Even if I don’t have specific context, they feel comfortable sharing and getting my perspective.",
                 type: ROLES.PRINCIPLE
             }
         ]
     }
 ];
 
-export default {quizQuestions, ROLES};
+export default { quizQuestions, ROLES };
